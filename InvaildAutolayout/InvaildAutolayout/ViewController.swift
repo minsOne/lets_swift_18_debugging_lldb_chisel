c88caaa0bec67c2127a04c17ecb93c2511c640ea
minsone <cancoffee7+github@gmail.com>
2018-11-04 22:50:07 +0900

    add files

---
+++ InvaildAutolayout/InvaildAutolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InvaildAutolayout
+//
+//  Created by jungmin ahn on 2018. 10. 19..
+//  Copyright © 2018년 jungmin ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TableViewProject/TableViewProject/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  TableViewProject
+//
+//  Created by jungmin ahn on 2018. 10. 19..
+//  Copyright © 2018년 jungmin ahn. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
